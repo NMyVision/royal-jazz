28bbcef913812c7b0d50b1b23e04163c39bbaa61
M. Gambill <mgambill@nmyvision.com>
2025-04-23 04:10:58 -0400

    switch to vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <script setup>
-
-
+import Logo from './components/RoyalLogo.vue'
 const events = [
   {
-    image: "boney_james.jpg",
+    image: 'boney_james.jpg',
     ticket:
-      "https://www.tixr.com/groups/ampballantyne/events/boney-james-royal-summer-jazz-series-139371",
+      'https://www.tixr.com/groups/ampballantyne/events/boney-james-royal-summer-jazz-series-139371',
   },
   {
-    image: "najee__regina_belle.jpg",
+    image: 'najee__regina_belle.jpg',
   },
   {
-    image: "will_downing.jpg",
+    image: 'will_downing.jpg',
   },
-];
+]
 //  \assets\img\najee__regina_belle.jpg
 //  /assets/img/najee__regina_belle.jpg
 </script>
@@ -22,23 +21,18 @@ const events = [
   <div class="bg-black/50">
     <section class="max-w-screen-xl mx-auto bg-black/80 px-4 sm:px-10">
       <div class="grid items-center justify-center p-10">
-        <LandingLogo class="h-[600px] fill-white" />
+        <Logo class="h-[600px] fill-white" />
       </div>
       <div class="md:flex items-center gap-4 py-10">
-        <template v-for="{ image } in events">
+        <template v-for="{ image } in events" :key="image">
           <a class="block" href="https://www.ampballantyne.com">
-            <img
-              :src="`/assets/img/${image}`"
-              alt="Image"
-              height="auto"
-              width="675px"
-            />
+            <img :src="`/src/assets/img/${image}`" alt="Image" height="auto" width="675px" />
           </a>
         </template>
       </div>
       <div class="p-10 text-center">
         <a
-          class="inline-block border-white/50 border-2 bg-[#b46a20] text-[#f1ed74]/90 py-4 px-8 uppercase font-medium rounded-full "
+          class="inline-block border-white/50 border-2 bg-[#b46a20] text-[#f1ed74]/90 py-4 px-8 uppercase font-medium rounded-full"
           href="https://www.ampballantyne.com"
           >Buy Tickets</a
         >
@@ -55,9 +49,7 @@ const events = [
             marginwidth="0"
             src="https://maps.google.com/maps?width=800&amp;height=400&amp;hl=en&amp;q=11115 upper ave&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
           ></iframe
-          ><a href="https://embed-googlemap.com"
-            >embed google maps in website</a
-          >
+          ><a href="https://embed-googlemap.com">embed google maps in website</a>
         </div>
       </div>
     </section>
@@ -65,16 +57,6 @@ const events = [
 </template>
 
 <style>
-html {
-  background-color: black;
-  background-image: url("/img/black concrete.jpg");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-blend-mode: multiply;
-  background-color: aliceblue;
-}
 
 .mapouter {
   position: relative;
--- src/components/RoyalLogo.vue
+++ src/components/RoyalLogo.vue
@@ -150,3 +150,4 @@
   </defs>
 </svg>
 </template>
+
